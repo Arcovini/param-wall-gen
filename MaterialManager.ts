e20451c81c7fb54d43ddfffb7da5b4b7b799f8b6
arcovinicius <vinicius.p.arcoverde@gmail.com>
2025-11-25 07:58:59 -0300

    Textures

--- MaterialManager.ts
+++ MaterialManager.ts
@@ -16,6 +16,11 @@ export class MaterialManager {
   private normalTexture: THREE.Texture | null = null;
   private ormTexture: THREE.Texture | null = null;
 
+  // Concrete textures for infill
+  private concreteBaseColorTexture: THREE.Texture | null = null;
+  private concreteNormalTexture: THREE.Texture | null = null;
+  private concreteArmTexture: THREE.Texture | null = null;
+
   private constructor() {
     this.textureLoader = new THREE.TextureLoader();
   }
@@ -31,12 +36,33 @@ export class MaterialManager {
   }
 
   /**
-   * Returns the shared Royal Blue material used for Lintels and Infill
+   * Returns the shared infill material with concrete textures
    */
   public getInfillMaterial(): THREE.MeshStandardMaterial {
     if (!this.infillMaterial) {
+      // Load concrete textures if not loaded
+      if (!this.concreteBaseColorTexture) {
+        this.concreteBaseColorTexture = this.textureLoader.load('/textures/concrete/concrete_layers_diff_1k.jpg');
+        this.concreteNormalTexture = this.textureLoader.load('/textures/concrete/concrete_layers_nor_gl_1k.jpg');
+        this.concreteArmTexture = this.textureLoader.load('/textures/concrete/concrete_layers_arm_1k.jpg');
+
+        // Configure textures
+        // Repeat 10x horizontally (along wall width) on front/back faces
+        [this.concreteBaseColorTexture, this.concreteNormalTexture, this.concreteArmTexture].forEach(texture => {
+          if (texture) {
+            texture.wrapS = THREE.RepeatWrapping;
+            texture.wrapT = THREE.RepeatWrapping;
+            texture.repeat.set(10, 1);
+          }
+        });
+      }
+
       this.infillMaterial = new THREE.MeshStandardMaterial({
-        color: 0x555555, // Dark grey (concrete-like)
+        map: this.concreteBaseColorTexture,
+        normalMap: this.concreteNormalTexture,
+        aoMap: this.concreteArmTexture,
+        roughnessMap: this.concreteArmTexture,
+        metalnessMap: this.concreteArmTexture,
         roughness: 0.9,
         metalness: 0.1
       });
@@ -65,9 +91,9 @@ export class MaterialManager {
     if (!this.brickMaterial) {
       // Load textures if not loaded
       if (!this.baseColorTexture) {
-        this.baseColorTexture = this.textureLoader.load('/textures/masonry/brick_baseColor.png');
-        this.normalTexture = this.textureLoader.load('/textures/masonry/brick_normal.png');
-        this.ormTexture = this.textureLoader.load('/textures/masonry/brick_occlusionRoughnessMetallic.png');
+        this.baseColorTexture = this.textureLoader.load('/textures/masonry/brick/redBrick_difuseAO.jpg');
+        this.normalTexture = this.textureLoader.load('/textures/masonry/brick/redBrick_Normal.jpg');
+        this.ormTexture = this.textureLoader.load('/textures/masonry/brick/redBrick_ARM.jpg');
 
         // Configure textures
         [this.baseColorTexture, this.normalTexture, this.ormTexture].forEach(texture => {
@@ -139,6 +165,18 @@ export class MaterialManager {
     if (this.ormTexture) {
       this.ormTexture.dispose();
       this.ormTexture = null;
+    }
+    if (this.concreteBaseColorTexture) {
+      this.concreteBaseColorTexture.dispose();
+      this.concreteBaseColorTexture = null;
+    }
+    if (this.concreteNormalTexture) {
+      this.concreteNormalTexture.dispose();
+      this.concreteNormalTexture = null;
+    }
+    if (this.concreteArmTexture) {
+      this.concreteArmTexture.dispose();
+      this.concreteArmTexture = null;
     }
   }
 }
